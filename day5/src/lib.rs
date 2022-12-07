bc7f4d807415c88c0a92599654e87ce998a4135f
Sebastian Sipos <sebastiansipos90@gmail.com>
2022-12-07 22:42:36 +0200

    day 5 - part 2

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -33,7 +33,7 @@ pub fn parse_stacks(stacks: &str) -> Result<Stacks, String> {
     let mut acc: Stacks = vec![vec![]; len];
     for current in t.iter().rev() {
         if current.len() != len {
-            return Err("smf".to_string());
+            return Err("Length mismatch".to_string());
         }
         current.iter().zip(&mut acc).for_each(|(a, b)| {
             if let Some(a) = a {
@@ -66,7 +66,7 @@ pub fn parse_input(input: &str) -> Result<(Stacks, Vec<Move>), String> {
     Ok((parse_stacks(stacks)?, parse_moves(moves)?))
 }
 
-pub fn process_moves(stacks: &Stacks, moves: &Vec<Move>) -> Stacks {
+pub fn process_moves_sequentially(stacks: &Stacks, moves: &Vec<Move>) -> Stacks {
     let mut stacks = stacks.clone();
     for (quantity, from, to) in moves {
         let mut tmp = vec![];
@@ -78,6 +78,23 @@ pub fn process_moves(stacks: &Stacks, moves: &Vec<Move>) -> Stacks {
         }
         let to = &mut stacks[*to - 1];
         for c in tmp.iter() {
+            to.push(*c);
+        }
+    }
+    stacks
+}
+pub fn process_moves_in_chunks(stacks: &Stacks, moves: &Vec<Move>) -> Stacks {
+    let mut stacks = stacks.clone();
+    for (quantity, from, to) in moves {
+        let mut tmp = vec![];
+        let from = &mut stacks[*from - 1];
+        for _ in 0..*quantity {
+            if let Some(c) = from.pop() {
+                tmp.push(c);
+            }
+        }
+        let to = &mut stacks[*to - 1];
+        for c in tmp.iter().rev() {
             to.push(*c);
         }
     }
@@ -114,7 +131,7 @@ move 2 from 2 to 1
 move 1 from 1 to 2"#;
             let (stacks, moves) = parse_input(input).unwrap();
 
-            let new_stacks = process_moves(&stacks, &moves);
+            let new_stacks = process_moves_sequentially(&stacks, &moves);
 
             assert_eq!(peek_stack(&new_stacks).as_str(), "CMZ");
         }
@@ -124,9 +141,52 @@ move 1 from 1 to 2"#;
             let input = read_to_string("input").unwrap();
             let (stacks, moves) = parse_input(&input).unwrap();
 
-            let new_stacks = process_moves(&stacks, &moves);
+            let new_stacks = process_moves_sequentially(&stacks, &moves);
 
             assert_eq!(peek_stack(&new_stacks).as_str(), "SHQWSRBDL");
+        }
+    }
+
+    mod part2 {
+        use std::fs::read_to_string;
+
+        fn peek_stack(stacks: &Stacks) -> String {
+            stacks
+                .iter()
+                .map(|stack| match stack.last() {
+                    None => ' ',
+                    Some(c) => *c,
+                })
+                .collect()
+        }
+
+        use super::*;
+        #[test]
+        fn example_works() {
+            let input = r#"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"#;
+            let (stacks, moves) = parse_input(input).unwrap();
+
+            let new_stacks = process_moves_in_chunks(&stacks, &moves);
+
+            assert_eq!(peek_stack(&new_stacks).as_str(), "MCD");
+        }
+
+        #[test]
+        fn input_works() {
+            let input = read_to_string("input").unwrap();
+            let (stacks, moves) = parse_input(&input).unwrap();
+
+            let new_stacks = process_moves_in_chunks(&stacks, &moves);
+
+            assert_eq!(peek_stack(&new_stacks).as_str(), "CDTQZHBRS");
         }
     }
 }
