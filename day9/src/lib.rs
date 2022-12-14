1e739400b380efff6d824e42bbb50a3eedbb16a0
Sebastian Sipos <sebastiansipos90@gmail.com>
2022-12-14 15:32:39 +0200

    day 9 - part 1

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -64,10 +64,10 @@ pub fn generate_history(data: &[(Direction, u32)]) -> Vec<(Vec2D, Vec2D)> {
         .flat_map(|(direction, magnitude)| vec![direction.to_vec(); *magnitude as usize])
         .fold(vec![], |mut history, (dx, dy)| {
             history.push(match history.last() {
-                Some(((hx, hy), tail)) => {
+                Some((old_head @ (hx, hy), tail)) => {
                     let head = (hx + dx, hy + dy);
-                    let tail = calculate_tail_position(head, *tail);
-                    dbg!((head, tail))
+                    let tail = calculate_tail_position(*old_head, head, *tail);
+                    (head, tail)
                 }
                 None => ((dx, dy), (dx, dy)),
             });
@@ -75,21 +75,15 @@ pub fn generate_history(data: &[(Direction, u32)]) -> Vec<(Vec2D, Vec2D)> {
         })
 }
 
-pub fn calculate_tail_position((hx, hy): Vec2D, (tx, ty): Vec2D) -> Vec2D {
-    let dx = hx - tx;
-    let dy = hx - hy;
-    let mut t2x = 0;
-    if dx.abs() == 2 {
-        t2x = tx + dx - 1;
+pub fn calculate_tail_position((hx, hy): Vec2D, (h2x, h2y): Vec2D, (tx, ty): Vec2D) -> Vec2D {
+    let dx = h2x.abs_diff(tx);
+    let dy = h2y.abs_diff(ty);
+
+    if dx > 1 || dy > 1 {
+        (hx, hy)
     } else {
-        t2x = tx;
+        (tx, ty)
     }
-    let t2y = ty + if dy == 2 {
-        1
-    } else {
-        0
-    };
-    (t2x, t2y)
 }
 
 #[cfg(test)]
@@ -97,8 +91,114 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
+    mod example_history {
+        fn parse_input((refx, refy): (i32, i32), input: &str) -> Vec<((i32, i32), (i32, i32))> {
+            input
+                .split("\n\n")
+                .map(|chunk| {
+                    let head = chunk
+                        .lines()
+                        .enumerate()
+                        .find_map(|(i, line)| {
+                            line.chars().enumerate().find_map(|(j, c)| match c {
+                                'H' => Some((refx + (i as i32), refy + (j as i32))),
+                                _ => None,
+                            })
+                        })
+                        .unwrap();
+                    let tail = chunk
+                        .lines()
+                        .enumerate()
+                        .find_map(|(i, line)| {
+                            line.chars().enumerate().find_map(|(j, c)| match c {
+                                'T' => Some((refx + (i as i32), refy + (j as i32))),
+                                _ => None,
+                            })
+                        })
+                        .unwrap_or(head);
+                    (head, tail)
+                })
+                .collect()
+        }
+
+        #[test]
+        #[ignore]
+        fn example_history() {
+            let input = std::fs::read_to_string("example_history").unwrap();
+            parse_input((0, 0), &input)
+                .windows(2)
+                .enumerate()
+                .for_each(|(i, window)| {
+                    let (old_head, old_tail) = window[0];
+                    let (head, tail) = window[1];
+                    println!(
+                        r#"({}, ({:?}, {:?}, {:?}, {:?})),"#,
+                        i, old_head, head, old_tail, tail
+                    );
+                });
+        }
+    }
+
     mod part1 {
         use super::*;
+        fn test_helper(input: &str) -> (Vec<(Vec2D, Vec2D)>, Vec<Vec2D>) {
+            let data = parse_input(input).unwrap();
+            let history = generate_history(&data);
+            // println!("history: {:?}", history);
+            // let (minx, maxx, miny, maxy) = history.iter().fold(
+            //     (
+            //         i32::max_value(),
+            //         i32::min_value(),
+            //         i32::max_value(),
+            //         i32::min_value(),
+            //     ),
+            //     |(minx, maxx, miny, maxy), ((hx, hy), (tx, ty))| {
+            //         (
+            //             [minx, *hx, *tx].iter().min().copied().unwrap(),
+            //             [maxx, *hx, *tx].iter().max().copied().unwrap(),
+            //             [miny, *hy, *ty].iter().min().copied().unwrap(),
+            //             [maxy, *hy, *ty].iter().max().copied().unwrap(),
+            //         )
+            //     },
+            // );
+            // println!("{minx}, {maxx}; {miny}, {maxy}");
+
+            // history.iter().for_each(|((hx, hy), (tx, ty))| {
+            //     println!(
+            //         "{}\n",
+            //         (minx + 1..maxx)
+            //             .rev()
+            //             .map(|i| {
+            //                 (miny..maxy + 1)
+            //                     .map(|j| {
+            //                         let mut c = '.';
+            //                         if i == *tx && j == *ty {
+            //                             c = 'T'
+            //                         }
+
+            //                         if i == *hx && j == *hy {
+            //                             c = 'H'
+            //                         }
+            //                         c
+            //                     })
+            //                     .collect::<String>()
+            //             })
+            //             .collect::<Vec<_>>()
+            //             .join("\n")
+            //     );
+            // });
+
+            let result: Vec<Vec2D> = history.iter().map(|(_, t)| t).fold(vec![], |mut acc, t| {
+                // println!("checking {:?}", t);
+                if !acc.contains(t) {
+                    // println!(" -> adding");
+                    acc.push(*t)
+                }
+                acc
+            });
+
+            (history, result)
+        }
         #[test]
         fn example_works() {
             let input = r#"R 4
@@ -110,59 +210,16 @@ D 1
 L 5
 R 2"#;
 
-            let data = parse_input(input).unwrap();
-            let history = generate_history(&data);
-            println!("history: {:?}", history);
-            let (minx, maxx, miny, maxy) = history.iter().fold(
-                (i32::max_value(), i32::min_value(), i32::max_value(), i32::min_value()),
-                |(minx, maxx, miny, maxy), ((hx, hy), (tx, ty))| {
-                    (
-                        [minx, *hx, *tx].iter().min().copied().unwrap(),
-                        [maxx, *hx, *tx].iter().max().copied().unwrap(),
-                        [miny, *hy, *ty].iter().min().copied().unwrap(),
-                        [maxy, *hy, *ty].iter().max().copied().unwrap(),
-                    )
-                },
-            );
-            println!("{minx}, {maxx}; {miny}, {maxy}");
+            let (_history, results) = test_helper(input);
 
-            history
-                .iter()
-                .for_each(|((hx, hy), (tx, ty))| {
-                    println!(
-                        "{}\n",
-                        (minx + 1..maxx)
-                            .rev()
-                            .map(|i| {
-                                (miny..maxy + 1)
-                                    .map(|j| {
-                                        let mut c = '.';
-                                        if i == *tx && j == *ty {
-                                            c = 'T'
-                                        }
+            assert_eq!(1 + results.len(), 13);
+        }
 
-                                        if i == *hx && j == *hy {
-                                            c = 'H'
-                                        }
-                                        c
-                                    })
-                                    .collect::<String>()
-                            })
-                            .collect::<Vec<_>>()
-                            .join("\n")
-                    );
-                });
-
-            let result: Vec<Vec2D> = history.iter().map(|(_, t)| t).fold(vec![], |mut acc, t| {
-                println!("checking {:?}", t);
-                if !acc.contains(t) {
-                    println!(" -> adding");
-                    acc.push(*t)
-                }
-                acc
-            });
-            println!("{:?}", result);
-            assert_eq!(result.len(), 13);
+        #[test]
+        fn input_works() {
+            let input = std::fs::read_to_string("input").unwrap();
+            let (_history, results) = test_helper(&input);
+            assert_eq!(1 + results.len(), 6190);
         }
     }
 
@@ -170,13 +227,19 @@ R 2"#;
     /// .TH.. -> .T.H. -> ..TH.
     /// .....    .....    .....
     ///
+    /// .....    .....    .....
+    /// ..HT. -> .H.T. -> .HT..
+    /// .....    .....    .....
+    ///
     /// ...    ...    ...
     /// .T.    .T.    ...
     /// .H. -> ... -> .T.
     /// ...    .H.    .H.
     /// ...    ...    ...
-    #[test_case((1, 3), (1, 1), (1, 2))]
-    #[test_case((3, 1), (1, 1), (2, 1))]
+    #[test_case("left to right", (1, 2), (1, 3), (1, 1), (1, 2))]
+    #[test_case("right to left", (1, 2), (1, 1), (1, 3), (1, 2))]
+    #[test_case("top to bottom", (2, 1), (3, 1), (1, 1), (2, 1))]
+    #[test_case("bottom to top", (2, 1), (3, 1), (1, 1), (2, 1))]
     ///
     /// .....    .....    .....
     /// .....    ..H..    ..H..
@@ -189,10 +252,57 @@ R 2"#;
     /// ..H.. -> ...H. -> ..TH.
     /// .T...    .T...    .....
     /// .....    .....    .....
-    #[test_case((1, 2), (3, 1), (2, 2))]
-    #[test_case((2, 3), (3, 1), (2, 2))]
-    fn tail_position_calculator(head: Vec2D, old_tail: Vec2D, new_tail: Vec2D) {
-        assert_eq!(calculate_tail_position(head, old_tail), new_tail);
+    #[test_case("diagonal", (2, 2), (1, 2), (3, 1), (2, 2))]
+    #[test_case("diagonal", (2, 2), (2, 3), (3, 1), (2, 2))]
+    ///
+    /// Provided scenario
+    fn tail_position_calculator(
+        _: &str,
+        old_head: Vec2D,
+        new_head: Vec2D,
+        old_tail: Vec2D,
+        new_tail: Vec2D,
+    ) {
+        assert_eq!(
+            calculate_tail_position(old_head, new_head, old_tail),
+            new_tail
+        );
+    }
+
+    #[test]
+    fn scenario_works() {
+        let moves = [
+            (0, ((4, 0), (4, 1), (4, 0), (4, 0))),
+            (1, ((4, 1), (4, 2), (4, 0), (4, 1))),
+            (2, ((4, 2), (4, 3), (4, 1), (4, 2))),
+            (3, ((4, 3), (4, 4), (4, 2), (4, 3))),
+            (4, ((4, 4), (3, 4), (4, 3), (4, 3))),
+            (5, ((3, 4), (2, 4), (4, 3), (3, 4))),
+            (6, ((2, 4), (1, 4), (3, 4), (2, 4))),
+            (7, ((1, 4), (0, 4), (2, 4), (1, 4))),
+            (8, ((0, 4), (0, 3), (1, 4), (1, 4))),
+            (9, ((0, 3), (0, 2), (1, 4), (0, 3))),
+            (10, ((0, 2), (0, 1), (0, 3), (0, 2))),
+            (11, ((0, 1), (1, 1), (0, 2), (0, 2))),
+            (12, ((1, 1), (1, 2), (0, 2), (0, 2))),
+            (13, ((1, 2), (1, 3), (0, 2), (0, 2))),
+            (14, ((1, 3), (1, 4), (0, 2), (1, 3))),
+            (15, ((1, 4), (1, 5), (1, 3), (1, 4))),
+            (16, ((1, 5), (2, 5), (1, 4), (1, 4))),
+            (17, ((2, 5), (2, 4), (1, 4), (1, 4))),
+            (18, ((2, 4), (2, 3), (1, 4), (1, 4))),
+            (19, ((2, 3), (2, 2), (1, 4), (2, 3))),
+            (20, ((2, 2), (2, 1), (2, 3), (2, 2))),
+            (21, ((2, 1), (2, 0), (2, 2), (2, 1))),
+            (22, ((2, 0), (2, 1), (2, 1), (2, 1))),
+            (23, ((2, 1), (2, 2), (2, 1), (2, 1))),
+        ];
+        for (_idx, (old_head, new_head, old_tail, new_tail)) in moves {
+            assert_eq!(
+                calculate_tail_position(old_head, new_head, old_tail),
+                new_tail
+            );
+        }
     }
 
     #[test_case(1, "R 4", Direction::Right, 4)]
