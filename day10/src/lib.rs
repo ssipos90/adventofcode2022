4df801ff9c645427f448b8b96cece6a2a63d94a8
Sebastian Sipos <sebastiansipos90@gmail.com>
2022-12-19 16:48:46 +0200

    day 10 - part 1

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Instruction {
     Noop,
     AddX(i32),
@@ -12,6 +12,55 @@ pub fn parse_instruction(line: &str) -> Result<Instruction, String> {
         )),
         _ => Err("Unknown pattern".to_string()),
     }
+}
+
+struct Registry {
+    x: i32,
+}
+
+pub fn interpretor<E: std::fmt::Display>(
+    mut lines: impl Iterator<Item = Result<String, E>>,
+) -> Result<i32, String> {
+    let mut registry = Registry { x: 1 };
+    let mut last_instruction: Option<Instruction> = None;
+    let mut signal: i32 = 0;
+    for cycle in 1.. {
+        // println!("\ncycle: {cycle}");
+        if cycle == 20 || (cycle + 20) % 40 == 0 {
+            // println!(" => calculating signal: {signal}");
+            signal += cycle * registry.x;
+        }
+        match last_instruction {
+            Some(instruction) => {
+                if let Instruction::AddX(x) = instruction {
+                    // println!("adding {x}");
+                    registry.x += x;
+                };
+                last_instruction = None;
+            }
+            None => {
+                match lines.next() {
+                    Some(Ok(line)) => match parse_instruction(line.as_str())? {
+                        Instruction::Noop => {
+                            // println!("noop");
+                        }
+                        instruction => {
+                            // println!("setting multi cycle instruction");
+                            last_instruction = Some(instruction);
+                        }
+                    },
+                    Some(Err(e)) => return Err(e.to_string()),
+                    None => {
+                        // println!("finished all instructions");
+                        break;
+                    }
+                }
+            }
+        };
+        // println!("registry value: {}", registry.x);
+    }
+
+    Ok(signal)
 }
 
 #[cfg(test)]
@@ -36,11 +85,28 @@ mod tests {
         assert!(parse_instruction(line).is_err());
     }
 
-    // mod part1 {
-    //     use super::*;
-    //     #[test]
-    //     fn example_works() {
-    //         let input = std::fs::read_to_string("example").unwrap();
-    //     }
-    // }
+    mod part1 {
+        use std::{
+            fs::File,
+            io::{BufRead, BufReader},
+        };
+
+        use super::*;
+        #[test]
+        fn example_works() {
+            let f = File::open("example").unwrap();
+            let buf = BufReader::new(f);
+
+            let result = interpretor(buf.lines()).unwrap();
+            assert_eq!(result, 13140)
+        }
+        #[test]
+        fn input_works() {
+            let f = File::open("input").unwrap();
+            let buf = BufReader::new(f);
+
+            let result = interpretor(buf.lines()).unwrap();
+            assert_eq!(result, 15120)
+        }
+    }
 }
