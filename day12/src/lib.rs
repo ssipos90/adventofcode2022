d3ec8d8cb022eae289f79e01ba7b9464abbe173f
Sebastian Sipos <sebastiansipos90@gmail.com>
2023-01-04 15:30:43 +0200

    day 12 - part 1

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -1,41 +1,41 @@
 use std::str::FromStr;
 
-use petgraph::algo::{astar, dijkstra};
-use petgraph::prelude::UnGraphMap;
+use petgraph::algo::astar;
+use petgraph::prelude::DiGraphMap;
 
 type Coord = (usize, usize);
 
 #[derive(Debug)]
 pub struct HeightMap {
     pub end: Coord,
-    pub graph: UnGraphMap<Coord, ()>,
+    pub graph: DiGraphMap<Coord, ()>,
     pub start: Coord,
 }
 
 impl HeightMap {
-    fn calculate_neighbors(x: usize, y: usize, m: usize, n: usize) -> [Option<Coord>; 3] {
-        let mut neighbors: [Option<Coord>; 3] = [None; 3];
-        // don't need to look up since up already looked down and vertices are unidirectional
-        // if x > 0 {
-        //     neighbors[0] = Some((x - 1, y));
-        // }
+    fn calculate_neighbors(x: usize, y: usize, m: usize, n: usize) -> [Option<Coord>; 4] {
+        let mut neighbors: [Option<Coord>; 4] = [None; 4];
+        // TODO: don't need to look up since up already looked down and vertices are unidirectional
+        if x > 0 {
+            neighbors[0] = Some((x - 1, y));
+        }
         if x + 1 < m {
-            neighbors[0] = Some((x + 1, y));
+            neighbors[1] = Some((x + 1, y));
         }
         if y > 0 {
-            neighbors[1] = Some((x, y - 1));
+            neighbors[2] = Some((x, y - 1));
         }
         if y + 1 < n {
-            neighbors[2] = Some((x, y + 1));
+            neighbors[3] = Some((x, y + 1));
         }
         neighbors
     }
 
-    pub fn shortest_paths_costs(&self) -> Option<usize> {
-        let map = dijkstra(&self.graph, self.start, Some(self.end), |_| 1);
+    // pub fn shortest_paths_costs(&self) -> Option<usize> {
+    //     let map = dijkstra(&self.graph, self.start, Some(self.end), |_| 1);
 
-        map.get(&self.end).cloned()
-    }
+    //     map.get(&self.end).cloned()
+    // }
 
     pub fn shortest_path(&self) -> Option<(usize, Vec<Coord>)> {
         astar(
@@ -63,11 +63,11 @@ impl FromStr for HeightMap {
                     .map(|(y, c)| match c {
                         'S' => {
                             start = Some((x, y));
-                            Ok('a' as isize - 97)
+                            Ok(0)
                         }
                         'E' => {
                             end = Some((x, y));
-                            Ok('z' as isize - 97)
+                            Ok(25)
                         }
                         c @ 'a'..='z' => Ok(c as isize - 97),
                         _ => Err("Char out of range".to_string()),
@@ -77,33 +77,43 @@ impl FromStr for HeightMap {
             .collect::<Result<Vec<Vec<isize>>, String>>()?;
 
         // for r in v.iter() {
-        //     println!("{}", r.iter().map(|h| char::from_u32((*h as u32) + 97).unwrap().to_string()).collect::<String>());
+        //     println!(
+        //         "{}",
+        //         r.iter()
+        //             .map(|h| char::from_u32((*h as u32) + 97).unwrap().to_string())
+        //             .collect::<String>()
+        //     );
         // }
 
         let m = v.len();
         let n = v[0].len();
 
-        let graph = v.iter().enumerate().flat_map(|(x, line)| {
-            let v = &v;
-            line.iter().enumerate().flat_map(move |(y, c)| {
-                let height = *c;
-                Self::calculate_neighbors(x, y, m, n)
-                    .iter()
-                    .filter_map(move |neighbor| {
-                        neighbor.and_then(|neighbor @ (x1, y1)| {
+        let graph: Vec<(Coord, Coord)> = v
+            .iter()
+            .enumerate()
+            .flat_map(|(x, line)| {
+                let v = &v;
+                line.iter().enumerate().flat_map(move |(y, c)| {
+                    let height = *c + 1;
+                    // println!("{x} {y}: {height}");
+                    Self::calculate_neighbors(x, y, m, n)
+                        .iter()
+                        .filter_map(|neighbor| *neighbor)
+                        .filter_map(move |(x1, y1)| {
                             let neighbor_height = v[x1][y1];
-                            if height - 1 <= neighbor_height && height + 1 >= neighbor_height {
-                                Some(((x, y), neighbor))
+                            // println!("  {x1} {y1}: {neighbor_height} {}", height >= neighbor_height);
+                            if height >= neighbor_height {
+                                Some(((x, y), (x1, y1)))
                             } else {
                                 None
                             }
                         })
-                    })
-                    .collect::<Vec<(Coord, Coord)>>()
+                        .collect::<Vec<(Coord, Coord)>>()
+                })
             })
-        });
+            .collect();
 
-        let graph = UnGraphMap::from_iter(graph);
+        let graph = DiGraphMap::from_edges(graph);
 
         Ok(Self {
             start: start.ok_or_else(|| "Did not found the start position.".to_string())?,
@@ -118,28 +128,30 @@ mod tests {
     use super::*;
 
     mod part1 {
-        use std::fs::write;
+        //use std::fs::write;
 
-        use petgraph::dot::{Config, Dot};
+        //use petgraph::dot::{Config, Dot};
 
         use super::*;
         #[test]
         fn example_works() {
             let input = include_str!("../example");
             let graph = HeightMap::from_str(input).unwrap();
-            write(
-                "graph",
-                format!(
-                    "{:?}",
-                    Dot::with_config(&graph.graph, &[Config::EdgeNoLabel])
-                ),
-            )
-            .unwrap();
+            //write(
+            //    "graph",
+            //    format!(
+            //        "{:?}",
+            //        Dot::with_config(&graph.graph, &[Config::EdgeNoLabel])
+            //    ),
+            //)
+            //.unwrap();
 
-            let cost = graph.shortest_paths_costs().unwrap();
+            let (cost, _path) = graph.shortest_path().unwrap();
+            //println!("{:?}", _path);
 
             assert_eq!(cost, 31);
         }
+
         #[test]
         fn input_works() {
             let input = include_str!("../input");
@@ -147,7 +159,7 @@ mod tests {
 
             let (cost, _path) = graph.shortest_path().unwrap();
 
-            assert_eq!(cost, 31);
+            assert_eq!(cost, 520);
         }
     }
 }
