7d40bf28dc078d778e1a251423d04f680e8db015
Sebastian Sipos <sebastiansipos90@gmail.com>
2023-01-04 16:00:58 +0200

    day 12 - part 2

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -8,8 +8,8 @@ type Coord = (usize, usize);
 #[derive(Debug)]
 pub struct HeightMap {
     pub end: Coord,
-    pub graph: DiGraphMap<Coord, ()>,
     pub start: Coord,
+    height_map: Vec<Vec<isize>>,
 }
 
 impl HeightMap {
@@ -37,14 +37,57 @@ impl HeightMap {
     //     map.get(&self.end).cloned()
     // }
 
-    pub fn shortest_path(&self) -> Option<(usize, Vec<Coord>)> {
+    pub fn shortest_path_up(&self) -> Option<(usize, Vec<Coord>)> {
         astar(
-            &self.graph,
+            &self.build_graph(&|height, neighbor_height| height + 1 >= neighbor_height),
             self.start,
             |current| current == self.end,
             |_| 1,
             |_| 1,
         )
+    }
+
+    pub fn shortest_path_down(&self) -> Option<(usize, Vec<Coord>)> {
+        astar(
+            &self.build_graph(&|neighbor_height, height| height >= neighbor_height - 1),
+            self.end,
+            |(x, y)| self.height_map[x][y] == 0,
+            |_| 1,
+            |_| 1,
+        )
+    }
+
+    fn build_graph<C>(&self, f: &C) -> DiGraphMap<Coord, ()>
+    where
+        C: Fn(isize, isize) -> bool,
+    {
+        let m = self.height_map.len();
+        let n = self.height_map[0].len();
+
+        let graph: Vec<(Coord, Coord)> = self
+            .height_map
+            .iter()
+            .enumerate()
+            .flat_map(|(x, line)| {
+                line.iter().enumerate().flat_map(move |(y, c)| {
+                    let height = *c;
+                    // println!("{x} {y}: {height}");
+                    Self::calculate_neighbors(x, y, m, n)
+                        .iter()
+                        .filter_map(|neighbor| *neighbor)
+                        .filter_map(move |(x1, y1)| {
+                            if f(height, self.height_map[x1][y1]) {
+                                Some(((x, y), (x1, y1)))
+                            } else {
+                                None
+                            }
+                        })
+                        .collect::<Vec<(Coord, Coord)>>()
+                })
+            })
+            .collect();
+
+        DiGraphMap::from_edges(graph)
     }
 }
 
@@ -85,40 +128,10 @@ impl FromStr for HeightMap {
         //     );
         // }
 
-        let m = v.len();
-        let n = v[0].len();
-
-        let graph: Vec<(Coord, Coord)> = v
-            .iter()
-            .enumerate()
-            .flat_map(|(x, line)| {
-                let v = &v;
-                line.iter().enumerate().flat_map(move |(y, c)| {
-                    let height = *c + 1;
-                    // println!("{x} {y}: {height}");
-                    Self::calculate_neighbors(x, y, m, n)
-                        .iter()
-                        .filter_map(|neighbor| *neighbor)
-                        .filter_map(move |(x1, y1)| {
-                            let neighbor_height = v[x1][y1];
-                            // println!("  {x1} {y1}: {neighbor_height} {}", height >= neighbor_height);
-                            if height >= neighbor_height {
-                                Some(((x, y), (x1, y1)))
-                            } else {
-                                None
-                            }
-                        })
-                        .collect::<Vec<(Coord, Coord)>>()
-                })
-            })
-            .collect();
-
-        let graph = DiGraphMap::from_edges(graph);
-
         Ok(Self {
             start: start.ok_or_else(|| "Did not found the start position.".to_string())?,
             end: end.ok_or_else(|| "Did not found the end position.".to_string())?,
-            graph,
+            height_map: v,
         })
     }
 }
@@ -146,7 +159,7 @@ mod tests {
             //)
             //.unwrap();
 
-            let (cost, _path) = graph.shortest_path().unwrap();
+            let (cost, _path) = graph.shortest_path_up().unwrap();
             //println!("{:?}", _path);
 
             assert_eq!(cost, 31);
@@ -157,9 +170,33 @@ mod tests {
             let input = include_str!("../input");
             let graph = HeightMap::from_str(input).unwrap();
 
-            let (cost, _path) = graph.shortest_path().unwrap();
+            let (cost, _path) = graph.shortest_path_up().unwrap();
 
             assert_eq!(cost, 520);
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn example_works() {
+            let input = include_str!("../example");
+            let graph = HeightMap::from_str(input).unwrap();
+
+            let (cost, _path) = graph.shortest_path_down().unwrap();
+
+            assert_eq!(cost, 29);
+        }
+
+        #[test]
+        fn input_works() {
+            let input = include_str!("../input");
+            let graph = HeightMap::from_str(input).unwrap();
+
+            let (cost, _path) = graph.shortest_path_down().unwrap();
+
+            assert_eq!(cost, 508);
         }
     }
 }
