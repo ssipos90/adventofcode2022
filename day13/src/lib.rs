2dc6b523f5416a48fc0de739e85d311bb0c9edd1
Sebastian Sipos <sebastiansipos90@gmail.com>
2023-01-06 15:08:48 +0200

    day 13 - part 1

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -3,23 +3,71 @@ use nom::{
     sequence::delimited, IResult,
 };
 
-pub fn process_signal(input: &str) -> Result<Vec<bool>, String> {
+#[derive(Debug, PartialEq)]
+enum PacketsOrder {
+    Wrong = 1,
+    Dunno,
+    Right,
+}
+
+impl PacketsOrder {
+    fn from_numbers(left: u32, right: u32) -> Self {
+        match left.cmp(&right) {
+            std::cmp::Ordering::Less => Self::Right,
+            std::cmp::Ordering::Equal => Self::Dunno,
+            std::cmp::Ordering::Greater => Self::Wrong,
+        }
+    }
+}
+
+fn check_order(left: Sequence, right: Sequence) -> PacketsOrder {
+    match (left, right) {
+        (Sequence::List(l), Sequence::List(r)) => check_list_order(l, r),
+        (Sequence::List(l), r @ Sequence::Number(_)) => check_list_order(l, vec![r]),
+        (l @ Sequence::Number(_), Sequence::List(r)) => check_list_order(vec![l], r),
+        (Sequence::Number(l), Sequence::Number(r)) => PacketsOrder::from_numbers(l, r),
+    }
+}
+
+fn check_list_order(left: Vec<Sequence>, right: Vec<Sequence>) -> PacketsOrder {
+    let llen = left.len();
+    let rlen = right.len();
+    let result =
+        left.into_iter()
+            .zip(right)
+            .find_map(|(left, right)| match check_order(left, right) {
+                PacketsOrder::Dunno => None,
+                o => Some(o),
+            });
+
+    match result {
+        Some(o) => o,
+        None => PacketsOrder::from_numbers(llen as u32, rlen as u32),
+    }
+}
+
+pub fn parse_signal(input: &str) -> Result<Vec<(Sequence, Sequence)>, String> {
     input
         .split("\n\n")
         .map(|p| {
             p.split_once('\n')
                 .ok_or_else(|| "Failed to split pair.".to_string())
                 .and_then(|(a, b)| {
-                    let _left = process_packet(a)?;
-                    let _right = process_packet(b)?;
-
-                    Ok(true)
+                    let left = parse_packet(a)?;
+                    let right = parse_packet(b)?;
+                    Ok((left, right))
                 })
         })
         .collect()
 }
 
-enum Sequence {
+pub fn check_signal(list: Vec<(Sequence, Sequence)>) -> Vec<bool> {
+    list.into_iter()
+        .map(|(left, right)| check_order(left, right) == PacketsOrder::Right)
+        .collect()
+}
+
+pub enum Sequence {
     List(Vec<Sequence>),
     Number(u32),
 }
@@ -45,18 +93,18 @@ impl ToString for Sequence {
     }
 }
 
-fn process_list(input: &str) -> IResult<&str, Vec<Sequence>> {
-    delimited(tag("["), separated_list0(tag(","), process_item), tag("]"))(input)
+fn parse_list(input: &str) -> IResult<&str, Vec<Sequence>> {
+    delimited(tag("["), separated_list0(tag(","), parse_item), tag("]"))(input)
 }
 
-fn process_item(input: &str) -> IResult<&str, Sequence> {
+fn parse_item(input: &str) -> IResult<&str, Sequence> {
     let number = map(nom::character::complete::u32, Sequence::Number);
 
-    alt((number, map(process_list, Sequence::List)))(input)
+    alt((number, map(parse_list, Sequence::List)))(input)
 }
 
-fn process_packet(input: &str) -> Result<Sequence, String> {
-    let (_, a) = process_item(input).map_err(|e| e.to_string())?;
+fn parse_packet(input: &str) -> Result<Sequence, String> {
+    let (_, a) = parse_item(input).map_err(|e| e.to_string())?;
 
     Ok(a)
 }
@@ -82,25 +130,62 @@ mod tests {
     #[test_case('n', "[[]]")]
     #[test_case('o', "[1,[2,[3,[4,[5,6,7]]]],8,9]")]
     #[test_case('p', "[1,[2,[3,[4,[5,6,0]]]],8,9]")]
-    fn packet_processor(_: char, input: &str) {
-        let packet = process_packet(input).unwrap();
+    fn packet_parser(_: char, input: &str) {
+        let packet = parse_packet(input).unwrap();
         assert_eq!(packet.to_string().as_str(), input);
+    }
+
+    #[test_case("1", "2", PacketsOrder::Right)]
+    #[test_case("2", "1", PacketsOrder::Wrong)]
+    #[test_case("1", "1", PacketsOrder::Dunno)]
+    #[test_case("[1,1,3,1,1]", "[1,1,5,1,1]", PacketsOrder::Right)]
+    #[test_case("[[1],[2,3,4]]", "[[1],4]", PacketsOrder::Right)]
+    #[test_case("[9]", "[[8,7,6]]", PacketsOrder::Wrong)]
+    #[test_case("[[4,4],4,4]", "[[4,4],4,4,4]", PacketsOrder::Right)]
+    #[test_case("[7,7,7,7]", "[7,7,7]", PacketsOrder::Wrong)]
+    #[test_case("[]", "[3]", PacketsOrder::Right)]
+    #[test_case("[[[]]]", "[[]]", PacketsOrder::Wrong)]
+    #[test_case(
+        "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+        "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+        PacketsOrder::Wrong
+    )]
+    fn order_checker(left: &str, right: &str, order: PacketsOrder) {
+        let left = parse_packet(left).unwrap();
+        let right = parse_packet(right).unwrap();
+
+        assert_eq!(check_order(left, right), order);
     }
 
     mod part1 {
         use super::*;
 
+        fn test_helper(signal: Vec<bool>) -> usize {
+            signal
+                .iter()
+                .enumerate()
+                .filter_map(|(i, correct)| correct.then(|| i + 1))
+                .sum()
+        }
+
         #[test]
         fn example_works() {
             let input = include_str!("../example");
 
-            let result: usize = process_signal(input)
-                .unwrap()
-                .iter()
-                .enumerate()
-                .filter_map(|(i, correct)| correct.then(|| i + 1))
-                .sum();
+            let signal = parse_signal(input).unwrap();
+
+            let result: usize = test_helper(check_signal(signal));
             assert_eq!(result, 13);
+        }
+
+        #[test]
+        fn input_works() {
+            let input = include_str!("../input");
+
+            let signal = parse_signal(input).unwrap();
+
+            let result: usize = test_helper(check_signal(signal));
+            assert_eq!(result, 4809);
         }
     }
 }
