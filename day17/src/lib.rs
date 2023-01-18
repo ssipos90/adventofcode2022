ae0901ffdd693cd8b222184b8e9ddbed9af1a36b
Sebastian Sipos <sebastiansipos90@gmail.com>
2023-01-18 14:56:20 +0200

    day 17 wip

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -48,20 +48,28 @@ pub fn parse_currents(input: &str) -> Result<Vec<Current>, String> {
         .collect()
 }
 
-pub fn simulate(rocks: &Vec<Rock>, currents: &Vec<Current>, c: usize) -> Vec<Vec<u8>> {
+pub fn simulate(rocks: &[Rock], currents: &[Current], c: usize) -> Vec<Vec<u8>> {
     let mut rocks = rocks.iter().cycle();
     let mut currents = currents.iter().cycle();
 
     let mut cave = vec![];
-    (0..c).for_each(|i| {
+    (0..c).for_each(|_| {
         let prev_max_height = cave.len() as isize;
+        let mut max_x = 0;
         let mut rock: Rock = rocks
             .next()
             .unwrap()
             .iter()
-            .map(|(x, y)| (prev_max_height + x + 3, y + 2))
+            .map(|(x, y)| {
+                let x = prev_max_height + x + 3;
+                let y = y + 2;
+                max_x = max_x.max(x);
+                (x, y)
+            })
             .collect();
-        println!("{:?}", rock);
+
+        cave.resize(max_x as usize, vec![0; 7]);
+
         loop {
             let current = currents.next().unwrap();
             let (left, right) = rock
@@ -71,21 +79,10 @@ pub fn simulate(rocks: &Vec<Rock>, currents: &Vec<Current>, c: usize) -> Vec<Vec
             rock.iter_mut().for_each(|piece| {
                 piece.0 += 1;
                 piece.1 += match current {
-                    Current::Left => {
-                        if left > 0 {
-                            -1
-                        } else {
-                            0
-                        }
-                    }
-                    Current::Right => {
-                        if right - 1 > 7 {
-                            1
-                        } else {
-                            0
-                        }
-                    }
-                };
+                    Current::Left => (left > 0).then_some(-1),
+                    Current::Right => (right - 1 > 7).then_some(1),
+                }
+                .unwrap_or(0);
             });
         }
     });
@@ -113,7 +110,10 @@ mod tests {
         let currents = parse_currents(input).unwrap();
         assert_eq!(currents.len(), 40);
         assert_eq!(
-            &currents,
+            currents
+                .into_iter()
+                .map(|d| d as isize)
+                .collect::<Vec<isize>>(),
             &[
                 1, 1, 1, -1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, -1, -1, -1, 1, 1, 1,
                 -1, -1, -1, 1, -1, -1, -1, 1, 1, -1, 1, 1, -1, -1, 1, 1
@@ -131,9 +131,9 @@ mod tests {
             let input = include_str!("../example");
             let currents = parse_currents(input).unwrap();
 
-            let world = simulate(&rocks, &currents, 2022);
+            let _world = simulate(&rocks, &currents, 2022);
 
-            assert_eq!(world.iter().fold(0, |acc, rock| acc.max(rock[0].0)), 3068);
+            assert_eq!(0, 3068);
         }
     }
 }
